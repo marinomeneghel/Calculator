ee1415a66e1176e766844f11c795014f5269998e
Marino Meneghel <marino.meneghel@xpeppers.com>
2016-05-17 14:49:26 +0200

    Added graphic elements, actions and outlet

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,25 +1,11 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by Marino Meneghel on 5/17/16.
-//  Copyright © 2016 Marino Meneghel. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var calculatorLabel: UILabel!
+
+    @IBAction func buttonClicked(sender: UIButton) {
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
