2cfd0deca257e8a6d75ad0353ffeceb798c02072
Marino Meneghel <marino.meneghel@xpeppers.com>
2016-05-18 14:26:44 +0200

    Added accumulator to show clicked numbers

---
+++ Calculator/Calculator.swift
@@ -0,0 +1,9 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by Marino Meneghel on 5/18/16.
+//  Copyright © 2016 Marino Meneghel. All rights reserved.
+//
+
+import Foundation
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -2,10 +2,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var accumulator: String = String()
+    private let calculator = Calculator()
+    
     @IBOutlet weak var calculatorLabel: UILabel!
 
     @IBAction func buttonClicked(sender: UIButton) {
-    
+        accumulator =  accumulator + sender.currentTitle!
+        calculatorLabel.text = accumulator
     }
+    
+    @IBAction func operationClicked(sender: UIButton) {
+        let operation = sender.currentTitle!
+        calculator.performOperation(operation)
+        print(calculator.result())
+    }
+    
+    
 }
 
