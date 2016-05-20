cc8e8fb2d80753ebe00992b7716cabdca0ba12bf
Marino Meneghel <marino.meneghel@xpeppers.com>
2016-05-20 15:36:50 +0200

    Added Operation enum, operations dict and PendindBinaryOperation struct

--- Calculator/Calculator.swift
+++ Calculator/Calculator.swift
@@ -2,16 +2,48 @@ import Foundation
 
 class Calculator {
     
-    private var accumulator: Double = Double()
+    private var accumulator = Double()
+    private var pending: PendingBinaryOperationInfo?
     
     func setOperand(operand: Double) {
         accumulator = operand
     }
     
-    func performOperation(operation: String) {
-        switch operation {
-        case "√": accumulator = sqrt(accumulator); break
-        default: break
+    enum Operation {
+        case Constant((Double))
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    let operations: Dictionary<String, Operation> = [
+        "℮": .Constant(M_E)
+    ]
+    
+    
+    func performOperation(mathematicalSymbol: String) {
+        if let operation = operations[mathematicalSymbol] {
+            switch operation {
+            case .Constant(let value):
+                accumulator = value
+                
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+                
+            case .BinaryOperation(let function):
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+                
+            case .Equals:
+                if pending != nil {
+                    accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+                    pending = nil
+                }
+            }
         }
     }
     
